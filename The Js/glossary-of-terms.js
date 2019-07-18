19ddcda5ae714aa331fb0429ed7662b239d52aaa
max-work-mac <max@dirty-martini.com>
2019-07-18 09:21:39 +0100

    moremore

--- The Js/glossary-of-terms.js
+++ The Js/glossary-of-terms.js
@@ -3,5 +3,4 @@
 //the pixel level of the bottom of the page = (window.scrollY + window.innerHeight)
 
 //the height of the whole browser window = window.outerHeight.
-
-//
+// the number of pixels from the top of the closest relatively positioned parent element. = .offsetTop
