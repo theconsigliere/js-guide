ae2f4d802e110626f2994fa9ae29abf272011e4d
max-work-mac <max@dirty-martini.com>
2019-07-18 09:20:02 +0100

    more changes

---
+++ The Js/glossary-of-terms.js
@@ -0,0 +1,7 @@
+//Scroll position = window.scrollY
+//the height of the browser window's viewport. = window.innerHeight
+//the pixel level of the bottom of the page = (window.scrollY + window.innerHeight)
+
+//the height of the whole browser window = window.outerHeight.
+
+//
